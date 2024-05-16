bf39eab120c192e57870f2dcca95de3dc2db1c7f
Reese <steven.schulz@outlook.com>
2024-05-16 17:44:28 +0200

    deleting unwanted files

--- testing/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gcc fusetest.c ../utils/enc_utils.c -o fusetest `pkg-config fuse --cflags --libs` -lsodium
-echo "./fusetest -f /home/foda-se/gitlab/transparent_enc_ba/testing/testmount1"
